8d66a7df185675135678b892874195cff6917732
Harsh Rajput <77964116+harshrajput688@users.noreply.github.com>
2022-04-17 00:40:00 -0700

    Create ReverseEachWord.java

---
+++ Strings and 2D Arrays/Assignment/ReverseEachWord.java
@@ -0,0 +1,56 @@
+/*
+Reverse Each Word
+
+Aadil has been provided with a sentence in the form of a string as a function parameter. The task is to implement a function so as to print the sentence such that each word in the sentence is reversed.
+Example:
+Input Sentence: "Hello, I am Aadil!"
+The expected output will print, ",olleH I ma !lidaA".
+Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. The input string represents the sentence given to Aadil.
+Output Format:
+The only line of output prints the sentence(string) such that each word in the sentence is reversed. 
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+Welcome to Coding Ninjas
+Sample Output 1:
+emocleW ot gnidoC sajniN
+Sample Input 2:
+Always indent your code
+Sample Output 2:
+syawlA tnedni ruoy edoc
+*/
+public class ReverseEachWord{
+
+	public static String reverseEachWord(String str, int start, int end) {
+		//Your code goes here
+        String reverse = "";
+        while(start < end){
+            reverse = str.charAt(start) + reverse;
+            ++start;
+        }
+        return reverse;
+	}
+    
+    public static String reverseEachWord(String str){
+        int n = str.length();
+        int previousSpaceIndex = -1;
+        
+        String ans = "";
+        
+        int i = 0;
+        for(;i<n;i++){
+            if(str.charAt(i) == ' '){
+                ans += (reverseEachWord(str, previousSpaceIndex + 1, i) + " ");
+                previousSpaceIndex = i;
+            }
+        }
+        
+        ans += (reverseEachWord(str, previousSpaceIndex + 1, i) + " ");
+        return ans;
+    }
+
+}
