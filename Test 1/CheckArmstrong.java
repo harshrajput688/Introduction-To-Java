467347131d6c8982b147ff7a71febf8bba5f579f
Harsh Rajput <77964116+harshrajput688@users.noreply.github.com>
2022-04-16 23:09:47 -0700

    Create CheckArmstrong.java

---
+++ Test 1/CheckArmstrong.java
@@ -0,0 +1,55 @@
+/*
+Check Armstrong
+
+Write a Program to determine if the given number is Armstrong number or not. Print true if number is armstrong, otherwise print false.
+An Armstrong number is a number (with digits n) such that the sum of its digits raised to nth power is equal to the number itself.
+For example,
+371, as 3^3 + 7^3 + 1^3 = 371
+1634, as 1^4 + 6^4 + 3^4 + 4^4 = 1634
+Input Format :
+Integer n
+Output Format :
+true or false
+Sample Input 1 :
+1
+Sample Output 1 :
+true
+Sample Input 2 :
+103
+Sample Output 2 :
+false
+*/
+import java.util.Scanner;
+public class CheckArmstrong {
+	
+	public static void main(String[] args) {
+		// Write your code here
+
+        Scanner s = new Scanner(System.in);
+		int number = s.nextInt();
+		int num = number;
+		int i =1;
+		int count = 0;
+		while(i<=num) {
+			num = num/10;
+			count++;
+		}
+		int originalNumber, remainder, result = 0;
+
+        originalNumber = number;
+
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, count);
+            originalNumber /= 10;
+        }
+
+        if(result == number)
+            System.out.println("true");
+        else
+            System.out.println("false");
+
+
+	}
+}
